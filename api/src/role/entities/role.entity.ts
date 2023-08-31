c3d243f11dfbee12af5fd53e67ec5a174142c9f6
Kelxety <llanzanarobinjohn@gmail.com>
2023-08-31 11:05:10 +0800

    fix role entity

--- api/src/role/entities/role.entity.ts
+++ api/src/role/entities/role.entity.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
 
 export class RoleEntity implements Role {
+  constructor(partial: Partial<RoleEntity>) {
+    Object.assign(this, partial);
+  }
   @ApiProperty()
   id: string;
 
--- api/src/role/role.controller.ts
+++ api/src/role/role.controller.ts
@@ -7,20 +7,18 @@ import {
   Param,
   Delete,
   Request,
-  UseGuards,
   Query,
 } from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
-import { ApiBearerAuth, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 import { Request as Req } from 'express';
-import { Roles } from 'src/auth/roles/roles.decorator';
-import { RoleGuard } from 'src/auth/role/role.guard';
-import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { QueryT, ResponseT } from 'src/lib/interface';
 import { RoleEntity } from './entities/role.entity';
 import { toBoolean } from 'src/lib/helper/cast.helper';
+import { CustomGlobalDecorator } from 'src/lib/decorators/global.decorators';
+import { Prisma } from '@prisma/client';
 
 @Controller('role')
 @ApiTags('system_roles')
@@ -28,8 +26,7 @@ export class RoleController {
   constructor(private readonly roleService: RoleService) {}
 
   @Post()
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard, RoleGuard)
+  @CustomGlobalDecorator(Prisma.RoleScalarFieldEnum, false, RoleEntity)
   create(@Request() request: Req, @Body() createRoleDto: CreateRoleDto) {
     return this.roleService.create(
       createRoleDto,
@@ -38,10 +35,7 @@ export class RoleController {
   }
 
   @Get()
-  @ApiBearerAuth()
-  @Roles('SUPERADMIN')
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @ApiOkResponse({ type: RoleEntity, isArray: true })
+  @CustomGlobalDecorator(Prisma.RoleScalarFieldEnum, true, RoleEntity)
   async findAll(@Query() query: QueryT): Promise<ResponseT<RoleEntity[]>> {
     const data = await this.roleService.findAll({
       data: query.filteredObject ? JSON.parse(query.filteredObject) : {},
@@ -58,35 +52,43 @@ export class RoleController {
   }
 
   @Get(':id')
-  @ApiBearerAuth()
-  @Roles('SUPERADMIN')
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @ApiOkResponse({})
-  findOne(@Param('id') id: string) {
-    return this.roleService.findOne(id);
+  @CustomGlobalDecorator(Prisma.RoleScalarFieldEnum, false, RoleEntity)
+  async findOne(@Param('id') id: string) {
+    const userCreated = await this.roleService.findOne(id);
+    return {
+      message: `Role successfuly created`,
+      data: new RoleEntity(userCreated),
+      total: 1,
+    };
   }
 
   @Patch(':id')
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles('SUPERADMIN')
-  update(
+  @CustomGlobalDecorator(Prisma.RoleScalarFieldEnum, false, RoleEntity)
+  async update(
     @Request() request: Req,
     @Param('id') id: string,
     @Body() updateRoleDto: UpdateRoleDto,
   ) {
-    return this.roleService.update(
+    const roleUpdated = await this.roleService.update(
       id,
       updateRoleDto,
       request?.headers?.authorization?.split('Bearer ')[1],
     );
+    return {
+      message: 'Role successfully updated',
+      data: new RoleEntity(roleUpdated),
+      total: 1,
+    };
   }
 
   @Delete(':id')
-  @ApiBearerAuth()
-  @UseGuards(JwtAuthGuard, RoleGuard)
-  @Roles('SUPERADMIN')
-  remove(@Param('id') id: string) {
-    return this.roleService.remove(id);
+  @CustomGlobalDecorator(Prisma.RoleScalarFieldEnum, false, RoleEntity)
+  async remove(@Param('id') id: string) {
+    await this.roleService.remove(id);
+    return {
+      message: 'Role successfully deleted',
+      data: {},
+      total: 0,
+    };
   }
 }
