12ef5f4eaa0272893ee9be90a37795e415188a34
Kelxety <llanzanarobinjohn@gmail.com>
2023-09-07 09:13:15 +0800

    update dto

--- packages/api/src/users/dto/update-user.dto.ts
+++ packages/api/src/users/dto/update-user.dto.ts
@@ -1,8 +1,9 @@
+import { PartialType } from '@nestjs/swagger';
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends CreateUserDto {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {}
 
 export class ChangePasswordDto {
   @IsString()
