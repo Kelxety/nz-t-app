f8e3c69efdb58c64f87a2a7a543621247a67e51e
rjcc <ryanjaycayabyab@gmail.com>
2023-09-28 10:21:23 +0800

    update schema

---
+++ prisma/migrations/20230927072639_rcv/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE `scm_receive` MODIFY `fy_code` INTEGER NULL,
+    MODIFY `rcv_refno` VARCHAR(30) NULL;
---
+++ prisma/migrations/20230927082430_itemdtl/migration.sql
@@ -0,0 +1,5 @@
+-- DropIndex
+DROP INDEX `scm_item_dtl_subitem_code_key` ON `scm_item_dtl`;
+
+-- DropIndex
+DROP INDEX `scm_item_dtl_subitem_name_key` ON `scm_item_dtl`;
