89c2877254d9fb19bab525067d6742f1813483c8
kelxety <llanzanarobinjohn@gmail.com>
2023-10-02 13:38:16 +0800

    remove duplicate migration file

--- prisma/migrations/20230929104851_scm_rcv/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE FULLTEXT INDEX `scm_receive_rcv_refno_purchaserequest_no_deliveryreceipt_no__idx` ON `scm_receive`(`rcv_refno`, `purchaserequest_no`, `deliveryreceipt_no`, `purchaseorder_no`);
