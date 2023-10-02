86b55303673d86a361eff19db1779faa975c78a2
kelxety <llanzanarobinjohn@gmail.com>
2023-10-02 16:40:41 +0800

    fix key for authCode

--- packages/pwa/src/app/pages/configuration/configuration-routing.ts
+++ packages/pwa/src/app/pages/configuration/configuration-routing.ts
@@ -7,38 +7,38 @@ export default [
   { path: 'location', data: { title: 'Location', key: 'location' }, loadComponent: () => import('./feature/location/location.component').then(c => c.LocationComponent) },
   {
     path: 'item-category',
-    data: { title: 'Item Category', key: 'itemCategory', breadcrumb: 'Item Category' },
+    data: { title: 'Item Category', key: 'default:configuration:item-category', breadcrumb: 'Item Category' },
     loadComponent: () => import('./feature/item-category/item-category.component').then(c => c.ItemCategoryComponent)
   },
   { path: 'item', data: { title: 'Item', key: 'item', breadcrumb: 'Item' }, loadComponent: () => import('./feature/item/item.component').then(c => c.ItemComponent) },
   {
     path: 'item-modal',
-    data: { title: 'Add Item', key: 'itemAddModal', breadcrumb: 'Add Item' },
+    data: { title: 'Add Item', key: 'default:configuration:item-modal', breadcrumb: 'Add Item' },
     loadComponent: () => import('./feature/item/item-modal/item-modal.component').then(c => c.ItemModalComponent)
   },
   {
     path: 'item-edit-modal/:id',
-    data: { title: 'Edit Item', key: 'itemEditModal', breadcrumb: 'Edit Item' },
+    data: { title: 'Edit Item', key: 'default:configuration:item-edit-modal', breadcrumb: 'Edit Item' },
     loadComponent: () => import('./feature/item/item-edit-modal/item-edit-modal.component').then(c => c.ItemEditModalComponent)
   },
   {
     path: 'unit',
-    data: { title: 'Unit', key: 'unit', breadcrumb: 'Unit' },
+    data: { title: 'Unit', key: 'default:configuration:unit', breadcrumb: 'Unit' },
     loadComponent: () => import('./feature/unit/unit.component').then(c => c.UnitComponent)
   },
   {
     path: 'warehouse',
-    data: { title: 'Warehouse', key: 'Warehouse', breadcrumb: 'Warehouse' },
+    data: { title: 'Warehouse', key: 'default:configuration:warehouse', breadcrumb: 'Warehouse' },
     loadComponent: () => import('./feature/warehouse/warehouse.component').then(c => c.WarehouseComponent)
   },
   {
     path: 'supplier',
-    data: { title: 'Supplier', key: 'supplier', breadcrumb: 'Supplier' },
+    data: { title: 'Supplier', key: 'default:configuration:supplier', breadcrumb: 'Supplier' },
     loadComponent: () => import('./feature/supplier/supplier.component').then(c => c.SupplierComponent)
   },
   {
     path: 'receive-mode',
-    data: { title: 'Receive mode', key: 'receive-mode', breadcrumb: 'Receive mode' },
+    data: { title: 'Receive mode', key: 'default:configuration:receive-mode', breadcrumb: 'Receive mode' },
     loadComponent: () => import('./feature/receive-mode/receive-mode.component').then(c => c.ReceiveModeComponent)
   }
 ] as Route[];
