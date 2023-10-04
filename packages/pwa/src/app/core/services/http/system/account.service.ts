5c5f674eeb93ef314bd7b59bf319f331fab8a12e
kelxety <llanzanarobinjohn@gmail.com>
2023-10-04 09:33:33 +0800

    fix change password url base path

--- packages/pwa/src/app/core/services/http/system/account.service.ts
+++ packages/pwa/src/app/core/services/http/system/account.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 
 import { Prisma, User } from '@prisma/client';
 import { HttpParamsService } from '@pwa/src/app/shared';
-import { QueryParams } from '@pwa/src/app/shared/interface';
+import { QueryParams, SearchParams } from '@pwa/src/app/shared/interface';
 import { ApiTypeService } from '@pwa/src/app/shared/services/api-type.service';
 import { ResType } from '@pwa/src/app/utils/types/return-types';
 
@@ -27,7 +27,7 @@ export class AccountService {
 
   constructor(private apiService: ApiTypeService, private httpParams: HttpParamsService) {}
 
-  getAccountList(params: QueryParams<Prisma.UserWhereInput>): Observable<ResType<User[]>> {
+  getAccountList(params: SearchParams<Prisma.UserWhereInput>): Observable<ResType<User[]>> {
     const parameters = this.httpParams.convert(params);
     return this.apiService.get<ResType<User[]>>(this.baseUrl, parameters);
   }
@@ -68,44 +68,8 @@ export class AccountService {
   }
 
   public editAccountPsd(params: UserPsd): Observable<string> {
-    const url = `${this.baseUrl}/${params.id}`;
+    const url = `${this.baseUrl}/changepass/${params.id}`;
     const data: Observable<string> = this.apiService.patch(url, params);
     return data;
   }
 }
-
-// constructor(public http: BaseHttpService) {}
-
-//   public getAccount(param: SearchCommonVO<User>): Observable<PageInfo<User>> {
-//     return this.http.post('/api/users/', param);
-//   }
-
-//   public getAccountList(param: SearchCommonVO<User>): Observable<User[]> {
-//     const headers = new HttpHeaders({
-//       'Content-Type': 'application/ld+json',
-//       Accept: 'application/ld+json'
-//     });
-//     const data: Observable<User[]> = this.http.get('/api/users/', { param, headers });
-//     return data;
-//   }
-
-//   public getAccountDetail(id: string): Observable<User> {
-//     return this.http.get(`/api/users/${id}/`);
-//   }
-
-//   public addAccount(param: User): Observable<void> {
-//     return this.http.post('/api/users/', param);
-//   }
-
-//   public delAccount(ids: string[]): Observable<void> {
-//     return this.http.post('/api/users/del/', { ids });
-//   }
-
-//   public editAccount(param: User): Observable<void> {
-//     return this.http.put('/api/users/', param);
-//   }
-
-//   public editAccountPsd(params: UserPsd): Observable<string> {
-//     const data: Observable<string> = this.http.patch(`/api/users/changepass/${params.id}`, params);
-//     return data;
-//   }
