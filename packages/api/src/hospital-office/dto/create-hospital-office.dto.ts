6024f422fca7d48c8526f5b88f53b56f3d2e9038
kelxety <llanzanarobinjohn@gmail.com>
2023-10-11 22:36:27 +0800

    added api property in hospital dto and entity

--- packages/api/src/hospital-office/dto/create-hospital-office.dto.ts
+++ packages/api/src/hospital-office/dto/create-hospital-office.dto.ts
@@ -1,8 +1,25 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateHospitalOfficeDto {
   id: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ required: true })
   officeName: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ required: true })
   officeAcro: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty({ required: true })
   state: string;
+
+  @ApiProperty()
   remarks: string;
   createdBy: string;
   createdAt: Date;
--- packages/api/src/hospital-office/entities/hospital-office.entity.ts
+++ packages/api/src/hospital-office/entities/hospital-office.entity.ts
@@ -1,13 +1,35 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { HospitalOffice } from '@prisma/client';
 
 export class HospitalOfficeEntity implements HospitalOffice {
+  constructor(partial: Partial<HospitalOffice>) {
+    Object.assign(this, partial);
+  }
+
+  @ApiProperty()
   id: string;
+
+  @ApiProperty()
   officeName: string;
+
+  @ApiProperty()
   officeAcro: string;
+
+  @ApiProperty()
   state: string;
+
+  @ApiProperty()
   remarks: string;
+
+  @ApiProperty()
   createdBy: string;
+
+  @ApiProperty()
   createdAt: Date;
+
+  @ApiProperty()
   updatedBy: string;
+
+  @ApiProperty()
   updatedAt: Date;
 }
